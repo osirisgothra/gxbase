5dba2bfca89c1ced0ab47a8037bb621db8007ad9
Gabriel T. Sharp <osirisgothra@hotmail.com>
2015-09-22 08:45:04 -0400

    depreciated branch

---
+++ README.md
@@ -0,0 +1,5 @@
+NOTICE
+======
+This branch has been kept for HISTORICAL reasons
+and is not meant to be used by anyone, please see
+the 'master' or 'dev' branch for real working copies!
---
+++ gxbase-start.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE: gxbase-start.pl
+#
+#        USAGE: ./gxbase-start.pl  
+#
+#  DESCRIPTION: Gxbase Starter (private)
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: YOUR NAME (), 
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 01/23/2015 07:16:12 AM
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+use utf8;
+use v5.20.1;
+use lib '/src/gxbase/lib';
+
+
+
+
+
